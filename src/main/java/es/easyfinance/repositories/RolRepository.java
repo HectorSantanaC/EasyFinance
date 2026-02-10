579ea16c774b7952e81cfd3cbd85e58fa803ce49
Héctor Santana <hector.santana.gc@gmail.com>
2026-02-10 20:13:47 +0000

    feat: crear RolRepository con query methods

---
+++ src/main/java/es/easyfinance/repositories/RolRepository.java
@@ -0,0 +1,20 @@
+package es.easyfinance.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import es.easyfinance.models.Rol;
+
+@Repository
+public interface RolRepository extends JpaRepository <Rol, Long> {
+	
+	Optional<Rol> findByNombre(String nombre);
+	
+    List<Rol> findByActivoTrue();
+    
+    boolean existsByNombre(String nombre);
+
+}
