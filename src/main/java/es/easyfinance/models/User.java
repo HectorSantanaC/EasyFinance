f9f78e210578d15acad6657f75ee30bd790a7315
Héctor Santana <hector.santana.gc@gmail.com>
2026-02-10 18:16:58 +0000

    feat: crear entidad User

---
+++ src/main/java/es/easyfinance/models/User.java
@@ -0,0 +1,140 @@
+package es.easyfinance.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	private String contrasena;
+	private String nombre;
+	private String apellidos;
+	private Rol rol;
+	private boolean activo = true;
+	private LocalDate fechaRegistro;
+	private LocalDateTime ultimoAcceso;
+	private Long creadoPor;
+	private LocalDateTime fechaCreacion;
+	private Long modificadoPor;
+	private LocalDateTime fechaModificacion;
+	
+	public User() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+	public LocalDate getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(LocalDate fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+
+	public LocalDateTime getUltimoAcceso() {
+		return ultimoAcceso;
+	}
+
+	public void setUltimoAcceso(LocalDateTime ultimoAcceso) {
+		this.ultimoAcceso = ultimoAcceso;
+	}
+
+	public Long getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(Long creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Long getModificadoPor() {
+		return modificadoPor;
+	}
+
+	public void setModificadoPor(Long modificadoPor) {
+		this.modificadoPor = modificadoPor;
+	}
+
+	public LocalDateTime getFechaModificacion() {
+		return fechaModificacion;
+	}
+
+	public void setFechaModificacion(LocalDateTime fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+
+}
