534a03ff1ca7bd8798b16378bbced780d275338e
Héctor Santana <hector.santana.gc@gmail.com>
2026-02-10 18:05:04 +0000

    feat: crear entidad Rol

---
+++ src/main/java/es/easyfinance/models/Rol.java
@@ -0,0 +1,85 @@
+package es.easyfinance.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Rol {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String descripcion;
+	private Long creadoPor;
+	private LocalDateTime fechaCreacion;
+	private Long modificadoPor;
+	private LocalDateTime fechaModificacion;
+
+	public Rol() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Long getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(Long creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Long getModificadoPor() {
+		return modificadoPor;
+	}
+
+	public void setModificadoPor(Long modificadoPor) {
+		this.modificadoPor = modificadoPor;
+	}
+
+	public LocalDateTime getFechaModificacion() {
+		return fechaModificacion;
+	}
+
+	public void setFechaModificacion(LocalDateTime fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+	
+}
