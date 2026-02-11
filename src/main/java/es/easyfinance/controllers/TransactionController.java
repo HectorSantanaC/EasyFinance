b6333bc0a51f4c05d4058b415911f0f3fa3fc469
Héctor Santana <hector.santana.gc@gmail.com>
2026-02-11 20:44:43 +0000

    feat: crear TransactionController REST API

---
+++ src/main/java/es/easyfinance/controllers/TransactionController.java
@@ -0,0 +1,53 @@
+package es.easyfinance.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.easyfinance.models.Transaction;
+import es.easyfinance.services.TransactionService;
+
+@RestController
+@RequestMapping("/api/transacciones")
+public class TransactionController {
+	
+	@Autowired
+	private TransactionService transactionService;
+
+    @GetMapping
+    public List<Transaction> listarTodas() {
+    	return transactionService.listarTodas();
+    }
+    
+    @GetMapping("/{id}")
+    public Transaction buscarPorId(@PathVariable Long id) {
+    	return transactionService.buscarPorId(id);
+    }
+    
+    @PostMapping
+    public ResponseEntity<Transaction> crear(@RequestBody Transaction transaction) {
+    	return ResponseEntity.ok(transactionService.guardar(transaction));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Transaction> actualizar(@PathVariable Long id, @RequestBody Transaction transaction) {
+    	transaction.setId(id);
+    	return ResponseEntity.ok(transactionService.guardar(transaction));
+    }
+    
+    @DeleteMapping("/{id}") 
+    public ResponseEntity<Void> borrar(@PathVariable Long id){
+    	transactionService.borrar(id);
+    	return ResponseEntity.ok().build();
+    }
+
+}
