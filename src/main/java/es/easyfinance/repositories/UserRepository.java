577743707a0abd18b6c1a4648772c31becf47fc7
Héctor Santana <hector.santana.gc@gmail.com>
2026-02-14 17:52:22 +0000

    refactor: simplificar querie JPA

--- src/main/java/es/easyfinance/repositories/UserRepository.java
+++ src/main/java/es/easyfinance/repositories/UserRepository.java
@@ -3,8 +3,6 @@ package es.easyfinance.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import es.easyfinance.models.UserModel;
 
@@ -13,8 +11,8 @@ public interface UserRepository extends JpaRepository<UserModel, Long> {
     // Validación Registro
     boolean existsByEmail(String email);
 	
-    @Query("SELECT u FROM UserModel u WHERE u.email = :email")
-    UserModel findByEmail(@Param("email") String email);
+    // Buscar por email
+    UserModel findByEmail(String email);
     
     // Listar usuarios activos (Admin)
     List<UserModel> findByActivoTrue();
